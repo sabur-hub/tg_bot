c7ed3220d101d00f1af61dc7a1c1ea46d96fdd25
Outic <76915977+sabur-hub@users.noreply.github.com>
2022-07-01 20:02:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5556951936:AAG_JrhyzM-mn9VoC2gqhCLg5TuPjYDQafA"
 APP_URL = "https://registrstudent.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
+DB_URI = "postgres://wdcpwublxvdasd:cb1349553b3c8ba9364538b404e486da4dee113792384b9f28711327314994de@ec2-52-30-75-37.eu-west-1.compute.amazonaws.com:5432/d89qhs6ea1scsr"
